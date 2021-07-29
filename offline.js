2ac81c24ce48503683dce5355e7c41a12650a2b6
herobrick360 <53086599+herobrick360@users.noreply.github.com>
2021-07-29 14:44:37 -0500

    Added Game

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1627587763,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/obj_player-sheet0.png",
+		"images/obj_player-sheet1.png",
+		"images/tiledbackground.png",
+		"images/obj_food-sheet0.png",
+		"images/obj_projectile-sheet0.png",
+		"images/obj_block-sheet0.png",
+		"images/obj_adversary-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"Photon-Javascript_SDK.min.js"
+	]
+}
